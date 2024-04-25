e9e7eb46548bf1ebe8765275184d207d6fdd89f5
Silvino Escalona <silvinoje14@gmail.com>
2024-04-25 07:52:09 -0400

    .

--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -1,3 +1,4 @@
+import { Position } from "monaco-editor";
 import { CSSProperties } from "react";
 
 export const html = String.raw;
@@ -13,4 +14,59 @@ export const styles = (styles: CSSProperties) => {
     .join("");
 
   return `style="${value}"`;
+};
+
+interface SimplePosition {
+  line: number;
+  column: number;
+}
+
+export const getMatchingBrackets = (code: string): [SimplePosition, SimplePosition][] => {
+  const matches: [SimplePosition, SimplePosition][] = [];
+
+  const lines = code.split("\r\n");
+
+  let bracketLevel = 0;
+  let isQuoting = false;
+  let isDQuoting = false;
+  let isEscaping = false;
+
+  let start: SimplePosition = { line: 0, column: 0 };
+  let end: SimplePosition = { line: 0, column: 0 };
+
+  lines.forEach((line, lineNumber) => {
+    line.split("").forEach((char, column) => {
+      if (char === "\\") {
+        isEscaping = !isEscaping;
+        return;
+      }
+
+      if (char === '"' && !isEscaping && !isQuoting) {
+        isDQuoting = !isDQuoting;
+        return;
+      }
+
+      if (char === "'" && !isEscaping && !isDQuoting) {
+        isQuoting = !isQuoting;
+        return;
+      }
+
+      if (char === "{" && !isDQuoting && !isQuoting) {
+        start;
+        bracketLevel += 1;
+        return;
+      }
+
+      if (char === "}" && !isDQuoting && !isQuoting) {
+        bracketLevel -= 1;
+      }
+    });
+  });
+
+  return [
+    [
+      { line: 1, column: 1 },
+      { line: 1, column: 1 },
+    ],
+  ];
 };
