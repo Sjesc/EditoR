3911712eb18a5abc3cae0ae6c4cb1e0e19ab3686
Silvino Escalona <silvinoje14@gmail.com>
2024-04-26 08:59:30 -0400

    update favicon & title

--- src/main.ts
+++ src/main.ts
@@ -80,7 +80,6 @@ updateComponent(
             <div class="flex flex-col overflow-hidden">
               <div class="font-medium text-lg mb-1">Packages</div>
               <div id="${components.packages}" class="overflow-auto flex gap-2 flex-wrap"></div>
-              ${/* .packages(TRUE) */ ""}
             </div>
 
             <div class="flex flex-col overflow-hidden">
